c15f33cdd21c4de7589a289f574a7ce4c292c359
Eric Fiorani <efiorani305@gmail.com>
2018-05-12 16:30:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
 
-  attr_accessor :name, :genre, :artist
-
-  def initialize(name, genre)
-    @name = name
-    @genre = genre
-  end
+  # attr_accessor :name, :genre, :artist
+  # 
+  # def initialize(name, genre)
+  #   @name = name
+  #   @genre = genre
+  # end
 
 end
