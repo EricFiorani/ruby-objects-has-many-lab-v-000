157cc8db518f1476bab916dd3f55fedf6c9658f5
Eric Fiorani <efiorani305@gmail.com>
2018-05-12 16:27:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,12 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @songs << song
+    # @songs << song
   end
 
   def add_song(song)
     @songs << song
+  end
 
   def songs
     @songs
